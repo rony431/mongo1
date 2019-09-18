d51b9937fa92f5323c5455f6d3fd0d0a02e3c05a
Ronald Estupiñan <ronaldestupinan@ZCOBTMH100224-RonaldEstupinan.local>
2019-09-18 18:59:46 -0500

    first commit

--- app.js
+++ app.js
@@ -6,17 +6,52 @@ mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost:27017/mongo-1',
 
 mongoose.connection.on("error", function(e){console.error(e)})
 var schema = mongoose.Schema({
-    name: { type: String, default:"Anónimo"},
-    date: { type: Date, default: Date.now() },
-  });
-          // definimos el modelo
- var Visitor = mongoose.model("Visitor", schema);
-
-app.get('/', (req, res) => {
-
-    Visitor.create({ name: req.query.name}, function(err) {
-      if (err) return console.error(err);
-    });
-  res.send("<h1>El visitante fue almacenado con éxito</h1>")
+  name: { type: String, default:"Anónimo"},
+  count: { type: Number, default: 1 },
 });
+// definimos el modelo
+var Visitor = mongoose.model("Visitor", schema);
+var initialHeader = "<html><head></head><body><table><tr><th>Id</th><th>Name</th><th>Visits</th></tr>";
+var finalCloser = "</table></body></html>"
+app.get('/', (req, res) => {
+ 
+  let nombre = "";
+  if (req.query.name||req.query.name != null){
+     nombre = req.query.name
+  }else{
+     nombre = "Anónimo" 
+  } 
+  Visitor.findOne({ name: nombre }, function(err, visitor) {
+    if (err) return console.error(err)
+      if(visitor){  
+        visitor.count += 1;
+        visitor.save(function(err) {
+          if (err) return console.error(err);
+          Visitor.find(function(err, visitors) {
+            if (err) return console.error(err);
+            var result = '';
+            for (let i =0; i < visitors.length; i++){
+               result = result + "<tr><td>"+visitors[i]._id+"</td><td>"+visitors[i].name+"</td><td>"+visitors[i].count+"</td></tr>"
+            }
+            res.send(initialHeader+result+finalCloser)
+          });  
+        });
+      }else{
+        Visitor.create({ name: nombre}, function(err) {
+           if (err) return console.error(err);
+          Visitor.find(function(err, visitors) {
+            if (err) return console.error(err);
+            var result= "";
+            for (let i =0; i < visitors.length; i++){
+               result = result + "<tr><td>"+visitors[i]._id+"</td><td>"+visitors[i].name+"</td><td>"+visitors[i].count+"</td></tr>"
+            }
+            res.send(initialHeader+result+finalCloser)
+          });  
+        });
+      }
+    }); 
+});
+
 app.listen(3000, () => console.log('Listening on port 3000!'));
+
+

c4a3a70f812d0364a0d51fbab9e12bd91e75dccf
Ronald Estupiñan <ronaldestupinan@ZCOBTMH100224-RonaldEstupinan.local>
2019-09-26 20:26:02 -0500

    authen

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express')
 var mongoose = require("mongoose");
+var bcrypt = require('bcrypt')
+var cookieSession = require('cookie-session')
 const app = express()
 app.set('view engine', 'pug');
 app.set('views', 'views');
@@ -9,28 +11,58 @@ mongoose.connection.on("error", function(e){console.error(e)})
 var schema = mongoose.Schema({
   name: { type: String, required:true},
   email: { type: String, required:true},
+  password: {type:String, required:true}
 
 });
 // definimos el modelo
 var Visitor = mongoose.model("Visitor", schema);
 var rows= '';
+app.use(express.urlencoded())
+app.use(cookieSession({
+  secret: 'prueba',
+  maxAge: 24 * 60 * 60 * 1000
+}));
 app.get('/', (req, res) => {
+  if(!req.session.userId){
+    res.redirect('/login')
+  }
   Visitor.find(function(err, visitors) {
     if (err) return console.error(err);
     res.render('table_home',{visitors: visitors});  
   }); 
+
+});
+app.get('/login', (req, res) => {
+  res.render('table_login');
 });
 app.get('/register', (req, res) => {
   res.render('index');
- 
 });
-app.use(express.urlencoded())
+app.get('/logout', (req, res) => {
+  req.session = null;
+  res.redirect('/')
+});
+app.post('/login', (req, res) => {
+  const emailFormLogin = req.body.email
+  const passFormLogin = req.body.password
+  
+  Visitor.findOne({ "email": emailFormLogin }, function(err, visitor) {
+    if (err) return console.error(err);
+    bcrypt.compare(passFormLogin, visitor.password).then(function(res){
+    req.session.userId = visitor._id;  
+    });
+    res.redirect('/')
+  });
+  
+})
 app.post('/register', (req, res) => {
   const nameForm = req.body.username
   const emailForm = req.body.email
-
-  Visitor.create({ name: nameForm, email: emailForm }, function(err) {
-    if (err) return console.error(err);
+  const passForm = req.body.password
+  bcrypt.hash(passForm,10).then(function(hash){
+    Visitor.create({ name: nameForm, email: emailForm, password:hash }, function(err) {
+      if (err) return console.error(err);
+    });
   });
   res.redirect('/')
 })
